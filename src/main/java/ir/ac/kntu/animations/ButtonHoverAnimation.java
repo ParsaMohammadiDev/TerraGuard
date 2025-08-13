c011ea9bdd5cb7ea019d77feb252a68e23e1337d
Parsa Mohammadi <parsa.mohammadi@email.kntu.ac.ir>
2025-08-13 16:22:49 +0330

    feat: improve ButtonHoverAnimation

--- src/main/java/ir/ac/kntu/animations/ButtonHoverAnimation.java
+++ src/main/java/ir/ac/kntu/animations/ButtonHoverAnimation.java
@@ -6,14 +6,16 @@ import javafx.util.Duration;
 
 public class ButtonHoverAnimation implements Animation {
     private static final Duration DURATION = Duration.millis(200);
-    private static final double FINAL_SCALE = 1.1;
+    private static final Duration PRESSED_DURATION = Duration.millis(50);
+    private static final double HOVERED_SCALE = 1.1;
+    private static final double PRESSED_SCALE = 1.08;
     @Override
     public void animate(Node... nodes) {
         for (Node node : nodes) {
             node.setOnMouseEntered(event -> {
                 ScaleTransition zoomOut = new ScaleTransition(DURATION, node);
-                zoomOut.setToX(FINAL_SCALE);
-                zoomOut.setToY(FINAL_SCALE);
+                zoomOut.setToX(HOVERED_SCALE);
+                zoomOut.setToY(HOVERED_SCALE);
                 zoomOut.play();
             });
 
@@ -22,6 +24,20 @@ public class ButtonHoverAnimation implements Animation {
                 zoomIn.setToX(1);
                 zoomIn.setToY(1);
                 zoomIn.play();
+            });
+
+            node.setOnMousePressed(event -> {
+                ScaleTransition zoomIn = new ScaleTransition(PRESSED_DURATION, node);
+                zoomIn.setToX(PRESSED_SCALE);
+                zoomIn.setToY(PRESSED_SCALE);
+                zoomIn.play();
+            });
+
+            node.setOnMouseReleased(event -> {
+                ScaleTransition zoomOut = new ScaleTransition(PRESSED_DURATION, node);
+                zoomOut.setToX(HOVERED_SCALE);
+                zoomOut.setToY(HOVERED_SCALE);
+                zoomOut.play();
             });
         }
     }
