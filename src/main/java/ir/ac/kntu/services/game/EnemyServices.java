1030fe24b8ffecf8a89a5ccf11d28eb23a6de77d
Parsa Mohammadi <parsa.mohammadi@email.kntu.ac.ir>
2025-08-17 14:03:02 +0330

    feat: add EnemyManager and EnemyRenderer to EnemyServices facade class

--- src/main/java/ir/ac/kntu/services/game/EnemyServices.java
+++ src/main/java/ir/ac/kntu/services/game/EnemyServices.java
@@ -3,9 +3,18 @@ package ir.ac.kntu.services.game;
 import ir.ac.kntu.services.game.components.enemies.factories.EnemyFactory;
 import ir.ac.kntu.services.game.components.enemies.types.factories.EnemyTypeFactory;
 import ir.ac.kntu.services.game.components.pathfinders.PathFinder;
+import ir.ac.kntu.services.game.core.managers.EnemyManager;
+import ir.ac.kntu.services.game.core.spawners.EnemyRenderer;
 
 public interface EnemyServices {
     EnemyTypeFactory getEnemyTypeFactory();
     PathFinder getPathFinder();
     EnemyFactory getEnemyFactory();
+    EnemyRenderer getEnemyRenderer();
+    EnemyManager getEnemyManager();
+    void setEnemyTypeFactory(EnemyTypeFactory enemyTypeFactory);
+    void setPathFinder(PathFinder pathFinder);
+    void setEnemyFactory(EnemyFactory enemyFactory);
+    void setEnemyRenderer(EnemyRenderer enemyRenderer);
+    void setEnemyManager(EnemyManager enemyManager);
 }
--- src/main/java/ir/ac/kntu/services/game/KenneyEnemyServices.java
+++ src/main/java/ir/ac/kntu/services/game/KenneyEnemyServices.java
@@ -3,11 +3,15 @@ package ir.ac.kntu.services.game;
 import ir.ac.kntu.services.game.components.enemies.factories.EnemyFactory;
 import ir.ac.kntu.services.game.components.enemies.types.factories.EnemyTypeFactory;
 import ir.ac.kntu.services.game.components.pathfinders.PathFinder;
+import ir.ac.kntu.services.game.core.managers.EnemyManager;
+import ir.ac.kntu.services.game.core.spawners.EnemyRenderer;
 
 public class KenneyEnemyServices implements EnemyServices {
     private EnemyTypeFactory enemyTypeFactory;
     private PathFinder pathFinder;
     private EnemyFactory enemyFactory;
+    private EnemyRenderer enemyRenderer;
+    private EnemyManager enemyManager;
 
     @Override
     public EnemyTypeFactory getEnemyTypeFactory() {
@@ -22,5 +26,40 @@ public class KenneyEnemyServices implements EnemyServices {
     @Override
     public EnemyFactory getEnemyFactory() {
         return enemyFactory;
+    }
+
+    @Override
+    public EnemyRenderer getEnemyRenderer() {
+        return enemyRenderer;
+    }
+
+    @Override
+    public EnemyManager getEnemyManager() {
+        return enemyManager;
+    }
+
+    @Override
+    public void setEnemyTypeFactory(EnemyTypeFactory enemyTypeFactory) {
+        this.enemyTypeFactory = enemyTypeFactory;
+    }
+
+    @Override
+    public void setPathFinder(PathFinder pathFinder) {
+        this.pathFinder = pathFinder;
+    }
+
+    @Override
+    public void setEnemyFactory(EnemyFactory enemyFactory) {
+        this.enemyFactory = enemyFactory;
+    }
+
+    @Override
+    public void setEnemyRenderer(EnemyRenderer enemyRenderer) {
+        this.enemyRenderer = enemyRenderer;
+    }
+
+    @Override
+    public void setEnemyManager(EnemyManager enemyManager) {
+        this.enemyManager = enemyManager;
     }
 }
