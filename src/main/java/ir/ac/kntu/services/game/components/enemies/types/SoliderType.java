fd5cbafcb18866c03cd022bde8e50878b0e9a4b3
Parsa Mohammadi <parsa.mohammadi@email.kntu.ac.ir>
2025-10-23 14:15:21 +0330

    refactor: improve enemy resources directory

--- src/main/java/ir/ac/kntu/services/game/components/enemies/types/SoliderType.java
+++ src/main/java/ir/ac/kntu/services/game/components/enemies/types/SoliderType.java
@@ -3,9 +3,9 @@ package ir.ac.kntu.services.game.components.enemies.types;
 import javafx.scene.image.Image;
 
 public class SoliderType implements EnemyType {
-    private static final String SOLIDER_BASE_PATH = "/game/components/enemies/bases/solider.png";
+    private static final String SOLIDER_BASE_PATH = "/game/components/enemies/overlays/solider.png";
     private static final Image SOLIDER_BASE_IMAGE = new Image(SOLIDER_BASE_PATH);
-    private static final String SOLIDER_OVERLAY_PATH = "/game/components/enemies/overlays/empty.png";
+    private static final String SOLIDER_OVERLAY_PATH = "/game/components/enemies/bases/empty.png";
     private static final Image SOLIDER_OVERLAY_IMAGE = new Image(SOLIDER_OVERLAY_PATH);
 
     @Override
